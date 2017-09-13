44c9f3594bb0d75d0e488b5a35916c6f4b3e2a33
robsonpereiradasilva <robson.silva@novaandradina.org>
2017-09-12 21:19:12 -0400

    Criado o model Etapa

---
+++ app/Etapa.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Etapa extends Model
+{
+     protected $fillable = [
+    	'id',
+    	'nome',
+    	'descricao',
+    	'obsevacao',
+    	'verificacao',
+    	'desativado'
+    ];
+}
