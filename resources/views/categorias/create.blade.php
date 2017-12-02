64f6ace8422bd850e28ef3386487cf0b15969ad8
Lucas de Oliveira Queiroz <lucasoliveiraoueiroz@gmail.com>
2017-12-02 13:55:43 -0300

    ...

--- resources/views/categorias/create.blade.php
+++ resources/views/categorias/create.blade.php
@@ -1,41 +1,5 @@
-
-<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Nova Categoria</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-        <form>
-          <div class="form-group">
-            <label for="id" class="form-control-label">Código</label>
-            <input type="text" class="form-control" id="codigo" disabled>
-          </div>
-          <div class="form-group">
-            <label for="message-text" class="form-control-label">Nome</label>
-            <input type="text" class="form-control" id="nome">
-          </div>
-        </form>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Sair</button>
-        <button type="button" class="btn btn-primary">Gravar</button>
-      </div>
-    </div>
-  </div>
-</div>
-
-
-<script>
-  $('#exampleModal').on('show.bs.modal', function (event) {
-  var button = $(event.relatedTarget) 
-  var recipient = button.data('whatever') 
-  var modal = $(this)
-  modal.find('.modal-title').text('New message to ' + recipient)
-  modal.find('.modal-body input').val(recipient)
-});
-
-</script>
+<form action="{{route('categorias.store')}}" method="POST">
+    {{csrf_field()}}
+    <input type="text" name="nome" placeholder="NOme">
+    <input type="submit">
+</form>
