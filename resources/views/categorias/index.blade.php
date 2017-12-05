dc9c2945b7564803ad14a98d3b578893e8ab7b01
Robson Silva <robson.silva@novaandradina.org>
2017-12-05 16:40:39 +0000

    index.blade.php edited online with Bitbucket

--- resources/views/categorias/index.blade.php
+++ resources/views/categorias/index.blade.php
@@ -2,28 +2,56 @@
 
     
     @section('content')
-    <table class="table table-striped">
+ <form method="POST" action="{{route('categorias.store')}}">  
+    {{csrf_field()}}
+  <div class="form-row">
+    <div class="form-group col-md-11">      
+      <input type="text" name="nome" class="form-control" required="" placeholder="Criar nova Categoria">
+    </div>
+    <div class="form-group col-md-1">
+        <button type="submit" class="btn btn-primary">Salvar</button>
+    </div>
+    
+  </div>
+ 
+  
+</form>
+
+
+    <table class="table table-striped table-bordered">
     	<thead>
     		<tr>
     			<th>id</th>
     			<th>Nome Categoria</th>
-    			<th>Desativado(S/N)</th>
-    			<th>Editar</th>
-    			<th>Deletar</th>
+    			<th>Ação</th>
+    			
     		</tr>
     	</thead>
     	<tbody>
-    		{{--@foreach ($categorias as $categoria)--}}
-    		<tr>   			
-    			<th scope="row">
-					<td>Dados</td>
-					<td>Dados</td>
-					<td>Dados</td>
-					<td>Edit</td>
-					<td>Deletar</td>
-    			</th>   			
+    		@foreach ($categorias as $categoria)
+    		<tr>
+    			<td>
+                    {{$categoria->id}}         
+                </td>
+                <td>
+                    {{$categoria->nome}}
+                </td>
+                <td>
+                    <a class="btn btn-secondary" href="{{ route('categorias.edit',['id'=>$categoria->id]) }}" role="button">Editar</a>
+                   
+
+                    <a class="btn btn-danger" href="{{ route('categorias.destroy',['id'=>$categoria->id]) }}"
+                onclick="event.preventDefault();
+                document.getElementById('desativar-form{{$categoria->id}}').submit();">Desativar</a>
+                <form id="desativar-form{{$categoria->id}}" action="{{ route('categorias.destroy',['id'=>$categoria->id]) }}"   method="POST" style="display: none;">
+                {{ csrf_field()}}
+                {{method_field("DELETE")}}
+                </form>
+                </td>
     		</tr>
-{{--@endforeach--}}
+
+
+            @endforeach
     		
     	</tbody>
     </table>
